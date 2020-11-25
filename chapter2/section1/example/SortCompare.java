74d762d4d01d70fddc5f6946ad6c1095962e2e9c
zzybot <zzybot@163.com>
2020-11-25 23:08:04 +0800

    Update SortCompare.java

--- chapter2/section1/example/SortCompare.java
+++ chapter2/section1/example/SortCompare.java
@@ -1,5 +1,8 @@
 package chapter2.section1.example;
 
+import chapter2.section1.exercise.Ex24;
+import chapter2.section1.exercise.Ex26;
+import chapter2.tools.ArrayGenerator;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
@@ -10,8 +13,8 @@ import edu.princeton.cs.algs4.Stopwatch;
 public class SortCompare {
     /**
      * 记录算法运行时间
-     * */
-    public static double time(String alg, Double[] a) {
+     */
+    public static double time(String alg, Comparable[] a) {
         Stopwatch timer = new Stopwatch();
         if ("Insertion".equals(alg)) {
             Insertion.sort(a);
@@ -19,15 +22,18 @@ public class SortCompare {
         if ("Selection".equals(alg)) {
             Selection.sort(a);
         }
-        if ("Shell".equals(alg)){
+        if ("Shell".equals(alg)) {
             Shell.sort(a);
+        }
+        if ("InsertionGurdian".equals(alg)) {
+            Ex24.Insertion(a);
         }
         return timer.elapsedTime();
     }
 
     /**
      * 返回算法排序 t 个长度为 n 的数组的总用时
-     * */
+     */
     public static double timeRandomInput(String alg, int n, int t) {
         double total = 0.0;
         Double[] a = new Double[n];
@@ -36,7 +42,42 @@ public class SortCompare {
                 a[j] = StdRandom.uniform();
             }
             //测试一个数组
-            total += time(alg,a);
+            total += time(alg, a);
+        }
+        return total;
+    }
+
+    /**
+     * Ex27
+     */
+    public static double timeRandomInputBy2(String alg, int t) {
+        int n = 128;
+        double total = 0.0;
+        for (int i = 0; i < t; i++) {
+            Double[] a = new Double[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = StdRandom.uniform();
+            }
+            n *= 2;
+            total += time(alg, a);
+        }
+        return total;
+    }
+
+    /**
+     * Ex28，数组只能取两种值
+     */
+    public static double timeRandomInputTwoChoice(String alg, int n, int t) {
+        double total = 0.0;
+        for (int i = 0; i < t; i++) {
+            int[] arr = ArrayGenerator.intsVrg(n, 1, 2);
+            Integer[] a = new Integer[arr.length];
+
+            for (int j = 0; j < arr.length; j++) {
+                a[j] = arr[j];
+            }
+            //测试一个数组
+            total += time(alg, a);
         }
         return total;
     }
@@ -48,12 +89,11 @@ public class SortCompare {
         int t = Integer.parseInt(args[3]);
 
         //算法 1 运行总时间
-        double t1 = timeRandomInput(alg1,n,t);
+        double t1 = timeRandomInput(alg1, n, t);
         //算法 2 运行总时间
-        double t2 = timeRandomInput(alg2,n,t);
+        double t2 = timeRandomInput(alg2, n, t);
 
-
-        StdOut.printf("For %d random Doubles\n   %s is",n,alg1);
-        StdOut.printf(" %.1f times faster than %s\n",t2/t1,alg2);
+        StdOut.printf("For %d random Doubles\n   %s is", n, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
     }
 }
