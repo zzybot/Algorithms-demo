6c820a96659304f04a88b9a469dd1cc381a9df9b
zzybot <zzybot@163.com>
2020-12-07 00:05:40 +0800

    Create Ex27.java

---
+++ chapter2/section4/excerise/Ex27.java
@@ -0,0 +1,13 @@
+package chapter2.section4.excerise;
+/**
+ * 实现一个 min(); 方法，返回最小元素，时间和空间的要求是常数级别
+ * */
+public class Ex27 {
+
+    /**
+     * 定义一个最小值 min，每次插入新节点时先比较最小值，如果比最小值小就复制给 min
+     * 答案在 ResizingMaxPq 中
+     * */
+
+
+}
